669385f1f37d6c6cdb1a875e689673a52f7223a2
Pasha Sheik <pashasheik33@gmail.com>
2019-10-10 18:49:52 -0700

    css push

---
+++ seeds.sql
@@ -0,0 +1,10 @@
+USE cplayground_db;
+
+INSERT INTO user (u_name,u_password,u_email) VALUES
+("Harry","123","huseyintasci@yandex.com");
+
+
+INSERT INTO buttons (b_name, button, b_department) VALUES 
+("Div", "<div></div>","html"),
+("Body", "body{ }","css"),
+("Function", "function funname() { }","js");
